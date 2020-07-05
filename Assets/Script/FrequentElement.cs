ec9e30523ca002c2b852a9ceaa550421c43fd69b
madhviSadhu <39146979+madhviSadhu@users.noreply.github.com>
2020-07-05 16:42:37 +0530

    Unity 2018.4.2f1

---
+++ Assets/Script/FrequentElement.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System;
+namespace FrequentElement
+{
+    public class FrequentElement : MonoBehaviour
+    {
+        public int[] array;
+        void Start()
+        {
+            int maxOccurrences = foo(array);
+            Debug.Log("maximum occurrences..."+ maxOccurrences.ToString());
+        }
+        int foo(int[] a)
+        {
+            Array.Sort(a);
+            int max = 0, i = 0, element = 0;
+            while( i < a.Length)
+            {
+                int temp = 0; 
+                while( i+1 <a.Length && a[i+1] == a[i] )
+                {
+                    temp += 1;
+                    i += 1;
+                }
+
+                if ( max < temp )
+                {
+                    max = temp;
+                    element = a[i];
+                }
+                else if (max == temp )
+                {
+                    element = Math.Min(element, a[i]);
+                }
+                i++;
+            }
+            return element;
+        }
+    }
+}
---
+++ Assets/Script/Palindrome.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using UnityEngine;
+
+namespace Palindrome
+{
+    public class Palindrome : MonoBehaviour
+    {
+        public string str;
+        public int k;
+        void Start()
+        {
+            if (k > str.Length || k == str.Length)
+            {
+                Debug.Log("InvalidInput....Palindrome");
+            }
+            else
+            {
+                if (IsPalindrome(str, k))
+                {
+                    Debug.Log("IsPalindrome = true");
+                }
+                else
+                {
+                    Debug.Log("IsPalindrome = false");
+                }
+            }
+        }
+
+        private bool IsPalindrome(string s , int i )
+        {
+            int length = s.Length;
+            string r = Reverse(s);
+            int p = PalindromeSequence(s.ToCharArray(), r.ToCharArray(), length, length);
+            return (length - p <= i);
+        }
+        private string Reverse(string s)
+        {
+            char[] charArray = s.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+
+        private int PalindromeSequence(char[] s, char[] r, int m, int n)   
+        {
+            if (m == 0 || n == 0)
+            {
+              return 0;
+            }
+
+            if (s[m - 1] == r[n-1])
+            {
+              return 1 + PalindromeSequence(s, r, m - 1, n - 1);
+            }
+            else
+            {
+              return Math.Max(PalindromeSequence(s, r, m, n - 1), PalindromeSequence(s, r, m - 1, n));
+            }
+        }
+    }
+}
